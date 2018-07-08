aee70338bd31c81e42652bd5417017d43363da5b
qiuqiangkong <qiuqiangkong@gmail.com>
2018-07-08 14:58:33 +0100

    update

---
+++ models_pytorch.py
@@ -0,0 +1,186 @@
+import math
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def init_layer(layer):
+    """Initialize a Linear or Convolutional layer. """
+    
+    if layer.weight.ndimension() == 4:
+        (n_out, n_in, height, width) = layer.weight.size()
+        n = n_in * height * width
+        
+    elif layer.weight.ndimension() == 2:
+        (n_out, n) = layer.weight.size()
+
+    std = math.sqrt(2. / n)
+    scale = std * math.sqrt(3.)
+    layer.weight.data.uniform_(-scale, scale)
+
+    if layer.bias is not None:
+        layer.bias.data.fill_(0.)
+
+
+def init_bn(bn):
+    """Initialize a Batchnorm layer. """
+    
+    bn.bias.data.fill_(0.)
+    bn.weight.data.fill_(1.)
+    
+"""
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=None):
+        
+        super(ConvBlock, self).__init__()
+        self.stride = stride
+        
+        self.conv1 = nn.Conv2d(in_channels=in_channels, 
+                              out_channels=out_channels,
+                              kernel_size=(3, 3), stride=(1, 1),
+                              padding=(1, 1), bias=False)
+                              
+        self.conv2 = nn.Conv2d(in_channels=out_channels, 
+                              out_channels=out_channels,
+                              kernel_size=(3, 3), stride=(1, 1),
+                              padding=(1, 1), bias=False)
+                              
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        if self.stride is not None:
+            self.conv_stride = nn.Conv2d(in_channels=in_channels, 
+                                        out_channels=out_channels,
+                                        kernel_size=(3, 3), stride=stride,
+                                        padding=(1, 1), bias=False)
+                                        
+            self.bn_stride = nn.BatchNorm2d(out_channels)
+        
+        self.init_weights()
+        
+    def init_weights(self):
+        
+        init_layer(self.conv1)
+        init_layer(self.conv2)
+        init_bn(self.bn1)
+        init_bn(self.bn2)
+        
+        if self.stride is not None:
+            init_layer(self.conv_stride)
+            init_bn(self.bn_stride)
+        
+    def forward(self, input):
+        
+        x = input
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        
+        if self.stride is not None:
+            x = F.relu(self.bn_stride(self.conv_stride(x)))
+            
+        else:
+            x = F.max_pool2d(x, kernel_size=(2, 2), stride=(2, 2))
+        
+        return x
+    
+    
+class Vggish(nn.Module):
+    def __init__(self, classes_num, stride=None):
+        
+        super(Vggish, self).__init__()
+
+        
+        self.conv_block1 = ConvBlock(in_channels=1, out_channels=64, stride=stride)
+        self.conv_block2 = ConvBlock(in_channels=64, out_channels=128, stride=stride)
+        self.conv_block3 = ConvBlock(in_channels=128, out_channels=256, stride=stride)
+        self.conv_block4 = ConvBlock(in_channels=256, out_channels=512, stride=stride)
+
+        self.fc_final = nn.Linear(512, classes_num, bias=True)
+
+        self.init_weights()
+
+    def init_weights(self):
+
+        init_layer(self.fc_final)
+
+    def forward(self, input, return_bottleneck=False):
+        (_, seq_len, mel_bins) = input.shape
+
+        x = input.view(-1, 1, seq_len, mel_bins)
+
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = self.conv_block4(x)
+        '''(samples_num, feature_maps, time_steps, freq_num)'''
+
+        x = F.avg_pool2d(x, kernel_size=x.shape[2:])
+        x = x.view(x.shape[0:2])
+
+        x = F.log_softmax(self.fc_final(x), dim=-1)
+
+        return x
+"""
+    
+class BaselineCnn(nn.Module):
+    def __init__(self, classes_num):
+        
+        super(BaselineCnn, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64,
+                               kernel_size=(5, 5), stride=(2, 2),
+                               padding=(2, 2), bias=False)
+
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128,
+                               kernel_size=(5, 5), stride=(2, 2),
+                               padding=(2, 2), bias=False)
+
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256,
+                               kernel_size=(5, 5), stride=(2, 2),
+                               padding=(2, 2), bias=False)
+
+        self.conv4 = nn.Conv2d(in_channels=256, out_channels=512,
+                               kernel_size=(5, 5), stride=(2, 2),
+                               padding=(2, 2), bias=False)
+
+        self.fc1 = nn.Linear(512, classes_num, bias=True)
+
+        self.bn1 = nn.BatchNorm2d(64)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.bn4 = nn.BatchNorm2d(512)
+
+        self.init_weights()
+
+    def init_weights(self):
+
+        init_layer(self.conv1)
+        init_layer(self.conv2)
+        init_layer(self.conv3)
+        init_layer(self.conv4)
+        init_layer(self.fc1)
+
+        init_bn(self.bn1)
+        init_bn(self.bn2)
+        init_bn(self.bn3)
+        init_bn(self.bn4)
+
+    def forward(self, input, return_bottleneck=False):
+        (_, seq_len, mel_bins) = input.shape
+
+        x = input.view(-1, 1, seq_len, mel_bins)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        """(samples_num, feature_maps, time_steps, freq_num)"""
+
+        (x, _) = torch.max(x, dim=-1)
+
+        x = torch.mean(x, dim=-1)
+
+        x = F.log_softmax(self.fc1(x), dim=-1)
+
+        return x
