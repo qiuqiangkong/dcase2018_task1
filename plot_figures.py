134c0e0a29df4e2024061c0ab688ea430854b176
qiuqiangkong <qiuqiangkong@gmail.com>
2018-07-08 17:19:31 +0100

    update

--- plot_figures.py
+++ plot_figures.py
@@ -23,9 +23,6 @@ def plot_logmel(args):
     # Paths
     audio_names = os.listdir(audio_dir)
     
-    import crash
-    asdf
-    
     # Feature extractor
     feature_extractor = LogMelExtractor(sample_rate=sample_rate, 
                                         window_size=window_size, 
@@ -91,3 +88,6 @@ if __name__ == '__main__':
     
     if args.mode == 'plot_logmel':
         plot_logmel(args)
+        
+    else:
+        raise Exception("Incorrect arguments!")
